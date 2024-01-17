2cd5f09b398c16a32315086086754bd13ee8ad87
Bruno <brunohenrique0665@gmail.com>
2024-01-17 12:54:54 -0300

    creating repository and custom queries

---
+++ src/main/java/com/brunobandeira/controledegastos/repository/GastoRepository.java
@@ -0,0 +1,17 @@
+package com.brunobandeira.controledegastos.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brunobandeira.controledegastos.entity.Gasto;
+import com.brunobandeira.controledegastos.util.TipoDeGasto;
+
+public interface GastoRepository extends JpaRepository<Gasto, Long>{
+    
+    List<Gasto> findByNome(String nome);
+    List<Gasto> findByQuantidade(int quantidade);
+    List<Gasto> findByTipoDeGasto(TipoDeGasto tipoDeGasto);
+    List<Gasto> findByData(LocalDate data);
+}
